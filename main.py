d4cadd799daf20fb7187a3e6cbd87b50537623e0
Royz2123 <1Qazwsxdc>
2020-06-06 14:59:36 +0300

    Added GUI looking fineeee

--- main.py
+++ main.py
@@ -1,17 +1,69 @@
 import whatsapp_api
+from threading import Thread
+import easygui
 
 import time
 
+BUTTON_OPTIONS = [
+    ["Stop Scan", "Start Scan"],
+    ["Hide Scan", "Show Scan"],
+]
 
-def main():
-    whats_client = whatsapp_api.WhatsappClient(initialize=True)
+
+def gui_main(whats_client):
+    easygui.buttonbox(
+        (
+            "Welcome to the Whatsapp Hacker Control Panel!\n"
+            "Click the button below to get started"
+        ),
+        "WH4t5apP HAck3R", ["Initialize Scan"]
+    )
+    whats_client.init_scan = True
 
     while True:
+        buttons = list(whats_client.get_scan_mode())
+
+        for i in range(len(BUTTON_OPTIONS)):
+            buttons[i] = BUTTON_OPTIONS[i][not buttons[i]]
+
+        ans = easygui.buttonbox(
+            (
+                "Welcome to the Whatsapp Hacker Control Panel!\n"
+                "Choose one of the options below to get started"
+            ),
+            "WH4t5apP HAck3R", buttons + ["Kill Scan"]
+        )
+
+        print(ans)
+
+        if ans in BUTTON_OPTIONS[0]:
+            whats_client.play_scan = not whats_client.play_scan
+        elif ans in BUTTON_OPTIONS[1]:
+            whats_client.show_scan = not whats_client.show_scan
+        else:
+            whats_client.kill_scan = True
+            return
+
+
+def main():
+    whats_client = whatsapp_api.WhatsappClient()
+
+    p = Thread(target=gui_main, args=(whats_client,))
+    p.start()
+
+    while not whats_client.init_scan:
+        time.sleep(1)
+
+    whats_client.initialize(initialize=True)
+
+    while not whats_client.kill_scan:
         print("Starting contacts scan")
         whats_client.scan_users()
         time.sleep(0.5)
 
     whats_client.close_conn()
+
+    p.join()
 
 
 if __name__ == "__main__":
--- whatsapp_api.py
+++ whatsapp_api.py
@@ -11,42 +11,59 @@ import time
 import DB_API
 
 
-class WhatsappClient():
+class WhatsappClient:
     TEST_CONTACT = "אלון שראל תלפיות"
-    TEST_CONTACT = "אלון בויאנג'ו - 39"  # TODO: Special Characters
+    TEST_CONTACT = "אלון בויאנג'ו - 39"
     TEST_CONTACT = "רון סנה שלום"
     TEST_CONTACT = "רזיאל גרצמן המלך"
+
+
     # TEST_CONTACT = "אשר תלפיות"
     # TEST_CONTACT = "דניאל הר אבן תלפיות"
     # TEST_CONTACT = "אסף תלפיות"
     # TEST_CONTACT = "אסף היי ניסיון"
 
-    def __init__(self, minimize=False, prompt=False, initialize=True):
+    def __init__(self, show_scan=True):
+        # scan modes
+        self.show_scan = show_scan
+        self.play_scan = False
+        self.init_scan = False
+        self.kill_scan = False
+
+    def set_window(self):
+        if not self.show_scan:
+            self._driver.set_window_position(-2000, 0)
+        else:
+            self._driver.set_window_position(0, 0)
+
+    def initialize(self, initialize=True):
         options = webdriver.ChromeOptions()
 
-        if not minimize:
-            options.add_argument(r"user-data-dir=./user_data")
+        # if not minimize:
+        options.add_argument(r"user-data-dir=./user_data")
 
         self._driver = webdriver.Chrome(
             executable_path="./drivers/chromedriver5.exe",
             chrome_options=options
         )
 
-        if minimize:
-            self._driver.minimize_window()
+        self.set_window()
 
         # get the whatsapp window
         self._driver.get("https://web.whatsapp.com")
 
         # wait until page loads?
-        if prompt:
-            input("Scan QR Code, And then Enter")
-            print("Logged in")
-        else:
-            time.sleep(20)
-            input("ready?")
+        # if prompt:
+        #     input("Scan QR Code, And then Enter")
+        #     print("Logged in")
+        # else:
+        #     time.sleep(20)
+
         if initialize:
             DB_API.init_db(DB_API.TEST_NAMES, DB_API.TEST_HOURS)
+
+    def get_scan_mode(self):
+        return self.play_scan, self.show_scan
 
     def close_conn(self):
         self._driver.quit()
@@ -54,12 +71,22 @@ class WhatsappClient():
     def scan_users(self, user_lst=DB_API.TEST_NAMES):
         try:
             for contact_name in user_lst:
-                self.get_contact_time(contact_name)
-                DB_API.set_image(contact_name, self.get_contact_image(contact_name,True))
-        except Exception as e:
-            print(f"Scan failed, Error: {e}")
-            raise e
+                # wait until need to play scan
+                while not self.play_scan:
+                    time.sleep(0.5)
+                    self.set_window()
 
+                    if self.kill_scan:
+                        break
+
+                self.set_window()
+                if self.kill_scan:
+                    break
+
+                self.get_contact_time(contact_name)
+                DB_API.set_image(contact_name, self.get_contact_image(contact_name, True))
+        except Exception as e:
+            print(f"Scan failed")
 
     def open_contact(self, contact=TEST_CONTACT):
         contact_names = contact.split(" ")
@@ -67,12 +94,11 @@ class WhatsappClient():
         found = False
 
         while not found and last_index != 0:
-            print(last_index)
             contact_name = " ".join(contact_names[:last_index])
             print(f"Searching for {contact_name}")
 
             try:
-                inp_xpath_search = "//div[@class='_2S1VP copyable-text selectable-text']"
+                inp_xpath_search = "//div[@class='_3FRCZ copyable-text selectable-text']"
                 input_box_search = self._driver.find_element_by_xpath(inp_xpath_search)
                 input_box_search.click()
                 input_box_search.clear()
@@ -82,7 +108,7 @@ class WhatsappClient():
 
                 # inp_xpath_contact = "//div[@class='_3j7s9']"
                 selected_contact = self._driver.find_element_by_xpath(
-                    "//span[starts-with(@title, '" + contact_name + "')]"
+                    '//span[starts-with(@title, "' + contact_name + '")]'
                 )
                 selected_contact.click()
 
@@ -90,14 +116,14 @@ class WhatsappClient():
                 break
             except Exception as e:
                 print(f"Could not find contact name: {contact_name}")
-                print(e)
+                # print(e)
 
             last_index -= 1
 
         if last_index != 0:
             print(f"Found contact: {contact_name}")
         else:
-            print("Couldn't find contact...")
+            print("Couldn't find contact at all")
 
     def write_message(self, message="this is a test"):
         self.open_contact()
@@ -108,20 +134,22 @@ class WhatsappClient():
         input_box.send_keys(message + Keys.ENTER)
         time.sleep(1)
 
-    def get_contact_image(self, contact_name, opened=False): # TODO - opened
+    def get_contact_image(self, contact_name, opened=False):
         if not opened:
             self.open_contact(contact_name)
 
-        # TODO - image_xpath
-        image_xpath = "//div[@class='_1-iDe Wu52Z']//header[@class='_2y17h']//img[@class='Qgzj8 gqwaM _3FXB1']"
-        image_url= ""
+        image_xpath = "//header[@class='_1iFv8']//div[@class='m7liR']//div[@class='_1BjNO']//img[@class='_2goTk _1Jdop _3Whw5']"
+        # image_xpath = "//header[@class='_2y17h']"
+        image_url = ""
         try:
             image_url = self._driver.find_elements_by_xpath(image_xpath)[-1].get_attribute("src")
-            print(image_url)
-        except Exception as e: # TODO - default user
-            print ("#############################")
-            print(e)
-            print("#############################")
+            print(f"Found image for {contact_name}: {image_url}")
+        except Exception as e:
+            # print("#############################")
+            # print(e)
+            # print("#############################")
+            print(f"Couldn't find image for {contact_name}")
+            image_url = "default user"
         return image_url
 
     def get_contact_time(self, contact_name):
@@ -135,7 +163,7 @@ class WhatsappClient():
                 header_text = self._driver.find_element_by_xpath(header_xpath).text
 
                 if header_text == "click here for contact info":
-                    time.sleep(0.5) # TODO - less sleep
+                    time.sleep(0.5)
                 else:
                     break
 
@@ -143,10 +171,10 @@ class WhatsappClient():
                 last_seen = header_text.split("last seen ")[1]
 
                 DB_API.lastseen_update(contact_name, last_seen)
-                DB_API.not_online_now(contact_name)     # is needed?
+                DB_API.not_online_now(contact_name)  # is needed?
 
                 print("Last seen: ", contact_name, " ", last_seen)
-            elif header_text.startswith("online"):
+            elif header_text.startswith("online") or header_text.startswith("typing"):
                 DB_API.online_now(contact_name)
 
                 print("Online user: ", contact_name)
@@ -155,6 +183,5 @@ class WhatsappClient():
 
         except selenium.common.exceptions.NoSuchElementException as e:
             print("User not online, disabled last seen")
-            print(e)
+            # print(e)
             DB_API.not_online_now(contact_name)
-
